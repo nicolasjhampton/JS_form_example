90ba9c57e2de2bf637f30b60e7f16c7e61b00bc6
nicolasjhampton <nicolasjhampton@gmail.com>
2016-05-10 14:23:03 -0700

    selectChic version 2.2.0 finished with doc and initial configuration

--- js/selectChic.js
+++ js/selectChic.js
@@ -2,7 +2,7 @@
 'use strict';
 
 /*
- *  SelectChic Version 2.0.0
+ *  SelectChic Version 2.2.0
  *  Copyright Nicolas James Hampton 2016
  *  Style the "select" menus (drop down menus) on the form
  *  so they match the styling of the text fields.
@@ -12,63 +12,146 @@
   var host = {};
 
   var defaults = {
-    "backgroundColor": $('input').css('background-color'),
-    "backgroundImage": "url(https://upload.wikimedia.org/wikipedia/commons/f/f1/MediaWiki_Vector_skin_action_arrow.svg)",
-    "selectPadding": ".1em",
     "imageOffsetPixels": 20,
-    "selectDivString": '<div class="newSelect"></div>',
-    "selectListString": '<ul class="newSelectList"></ul>',
-    "optionString": '<li class="newOption"></li>',
-    "selectListStyles": {
+    "divClass": "newSelect",
+    "listClass": "newSelectList",
+    "optionClass": "newOption",
+    "matchString": "input",
+    "matchStyles": [
+      "background-color",
+      "padding-left",
+      "padding-right",
+      "border-right",
+      "border-left",
+      "border-top",
+      "border-bottom",
+      "border-radius"],
+    "selectDivBase": {
+      "background-repeat": "no-repeat"
+    },
+    "selectDivStyles": {
+      "background-image":  "url(https://upload.wikimedia.org/wikipedia/commons/f/f1/MediaWiki_Vector_skin_action_arrow.svg)"
+    },
+    "selectListBase": {
       "list-style": "none",
       "padding-left": "0px",
-      "padding-right": "50px"
+      "padding-right": "50px",
+      "margin-top": "0px",
+      "margin-bottom": "0px"
     },
-    "optionItemStyles": {
+    "selectListStyles": {},
+    "optionBase": {
       "display": "none",
       "border-bottom": "solid black 1px",
       "margin-bottom": "10px",
       "padding": "5px"
-    }
+    },
+    "optionStyles": {}
   };
 
-  $.configureSelect = function(options) {
+
+  /**
+   * Configuration method for SelectChic
+   *
+   * @param options: configuration object for all settings
+   *
+   * @returns jQuery array of all select elements on page
+   *
+   */
+  $.selectChic = function(options) {
 
     if(!options) { options = {}; }
     var config = $.extend( {}, defaults, options );
 
-    //host.selectElement = $object;
 
     Object.keys(config).map(function(setting) {
       host[setting] = config[setting];
     });
 
-    host.selectDivStyles = {
-      "background-color": host.backgroundColor,
-      "background-image": host.backgroundImage,
-      "background-repeat": "no-repeat",
-      "padding": host.selectPadding
-    }
+    $(window).resize(function() {
+      $('.' + host.divClass).each(function(){
+        $(this).resetBgPosition();
+      });
+    });
+
+    return $('select');
   }
 
 
+  /**
+   * jQuery method to hide current select element and
+   * replace it with an div element containing an
+   * unordered list linked to the select box.
+   *
+   * @param activeOptionIndex: index of option element
+   *                           to be displayed. Starts
+   *                           at zero.
+   *
+   * @returns jQuery object of newly created select box
+   *
+   */
+  $.fn.convertSelect = function(activeOptionIndex) {
+    var newSelect = $('<div class="' + host.divClass + '"></div>');
+    var selectList = $('<ul class="' + host.listClass + '"></ul>');
+    var selectElement = this;
 
+    newSelect.append(selectList);
+    selectList.css(host.selectListBase)
+              .css(host.selectListStyles);
+
+    selectElement.children().each(function(index) {
+
+      var optionLink = $('<li class="' + host.optionClass + '"></li>');
+
+      selectList.append(optionLink);
+      optionLink.css(host.optionBase)
+                .css(host.optionStyles)
+                .copyElement(this);
+
+      if(index === activeOptionIndex) {
+        optionLink.off()
+                  .show()
+                  .css("margin-bottom", "0px")
+                  .click(displayOptions);
+      }
+    });
+
+    selectElement.hide().after(newSelect);
+
+    return newSelect.copyStyles(host.matchString)
+                    .css(host.selectDivBase)
+                    .css(host.selectDivStyles)
+                    .resetBgPosition();
+
+  };
+
+
+  /**
+   * Function ran by click event on option elements
+   * on a open select menu. Closes the option menu,
+   * selects correct option in select box, and
+   * triggers select change event.
+   *
+   * @param e: click event object
+   *
+   */
   function optionSelect(e) {
     e.stopPropagation();
 
     var thisOptionItem = $(this);
     var selectDiv = thisOptionItem.parent().parent();
-    var imageOffset = selectDiv.width() - host.imageOffsetPixels;
     var selectBox = selectDiv.prev();
     var optionSelectorString = '[value="' + thisOptionItem.attr('value') + '"]';
     var selectBoxOptionSelected = selectBox.children(optionSelectorString);
 
     thisOptionItem.show()
+                  .css("margin-bottom", "0px")
                   .click(displayOptions)
                   .siblings().hide();
 
-    selectDiv.css("background-position", imageOffset)
-             .css(host.selectDivStyles);
+    selectDiv.css(host.selectDivBase)
+             .css(host.selectDivStyles)
+             .resetBgPosition()
 
     selectBoxOptionSelected.prop("selected", true);
 
@@ -76,14 +159,83 @@
   }
 
 
+  /**
+   * Function ran by click event on option elements
+   * on a closed select menu. Opens the option menu,
+   * attaches optionSelect click events on all option
+   * items, and repositions the select arrow and margins.
+   *
+   * @param e: click event object
+   *
+   */
   function displayOptions(e) {
     e.stopPropagation();
-    var that = $(this); // optionLink
-    that.off().click(optionSelect);
-    that.siblings().off().click(optionSelect).show('slow');
+    var that = $(this);
+    var selectDiv = that.parent().parent();
+    selectDiv.css("background-image", "");
+    that.off().click(optionSelect).css("margin-bottom", "10px");
+    that.siblings().off().click(optionSelect).show('slow', resetBgPositionWrapper);
   }
 
 
+  /**
+   * Function ran by resize event on window object,
+   * essentially a wrapper for resetBgPosition, in order
+   * to pass as a callback for timing purposes
+   *
+   */
+  function resetBgPositionWrapper() {
+    $(this).parent().parent().resetBgPosition();
+  }
+
+
+  /**
+   * jQuery method to reposition the background image
+   * based on the current width of the element and
+   * desired offset. For select arrow positioning.
+   *
+   * @returns jQuery object
+   *
+   */
+  $.fn.resetBgPosition = function() {
+    var imageOffset = this.width() - host.imageOffsetPixels;
+    this.css(
+      {
+        "background-position": imageOffset,
+        "background-image": host.selectDivStyles["background-image"]
+      });
+    return this;
+  }
+
+
+  /**
+   * jQuery method to copy all the css properties
+   * listed in the matchStyles array and apply them
+   * to the jQuery object running the function. Used
+   * to help style the selectDiv's.
+   *
+   * @param sender: DOM element or css selector
+   *
+   * @returns jQuery object
+   *
+   */
+  $.fn.copyStyles = function(sender) {
+    var obj = $(sender).css(host.matchStyles);
+    return $(this).css(obj);
+  }
+
+
+  /**
+   * jQuery method to copy all attributes and
+   * inner HTML on sender DOM element and apply
+   * them to the jQuery object calling the method.
+   * Used to fill in each new select option.
+   *
+   * @param sender: DOM element
+   *
+   * @returns jQuery object
+   *
+   */
   $.fn.copyElement = function(sender) {
     var that = this;
 
@@ -96,37 +248,6 @@
     that.html(sender.innerHTML);
 
     return that;
-  };
-
-
-  $.fn.convertSelect = function(activeOptionIndex) {
-    var newSelect = $(host.selectDivString);
-    var selectList = $(host.selectListString);
-    var selectElement = this;//$(host.selectElement);
-
-    newSelect.append(selectList);
-    selectList.css(host.selectListStyles);
-
-    selectElement.children().each(function(index) {
-
-      var optionLink = $(host.optionString);
-
-      selectList.append(optionLink);
-      optionLink.css(host.optionItemStyles)
-                .copyElement(this);
-
-      if(index === activeOptionIndex) {
-        optionLink.off()
-                  .show()
-                  .click(displayOptions);
-      }
-    });
-
-    selectElement.hide().after(newSelect);
-    // Style the new select div (note: browser must calculate image width in real time)
-    var imageOffset = newSelect.width() - host.imageOffsetPixels;
-    newSelect.css(host.selectDivStyles)
-             .css("background-position", imageOffset);
   };
 
 
